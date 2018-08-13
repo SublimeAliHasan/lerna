f569611263c3043db3860bd93fa8d89a07231105
SublimeAliHasan <ali.hasan@sublimeskinz.com>
2018-08-13 10:57:17 +0200

    ui packages

---
+++ sublime-ui-packages/packages/sublime-ui-select-field/make.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir package
+cp -R lib index.js package.json package
+version=`node -p "require('./package.json').version"`
+fileName="$1_$version.tgz"
+fileName= echo $fileName
+tar -cvzf "$fileName" package
+if [ ! -d ../../libs/$1/ ];
+then
+    mkdir ../../libs/$1
+fi
+mv "$fileName" ../../libs/$1/
+rm -R package
